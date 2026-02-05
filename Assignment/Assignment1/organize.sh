6c65ab1de78e04fcb094ad5a51e07af2ef3eb239
Abhay78888 <abhaybhadauria8999@gmail.com>
2026-02-05 12:47:32 +0530

    hello

---
+++ Assignment/Assignment1/organize.sh
@@ -0,0 +1,15 @@
+mkdir -p PDFs Images Text
+for file in *; do
+[ -f "$file" ] || continue
+case "$file" in 
+*.pdf)
+mv -- "$file" PDFs/
+;;
+*.jpg|*.png)
+mv -- "$file" Images/
+;;
+*.txt)
+mv -- "$file" Text/
+;;
+esac
+done
